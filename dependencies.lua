19ac73c576bff3538d14830475d26d305b482615
Tripiod8 <trdoty@mail.wvncc.edu>
2020-04-21 23:22:03 -0400

    Commit

---
+++ dependencies.lua
@@ -0,0 +1,15 @@
+-- Third Party Libraries
+anim8 = require('libs/anim8/anim8')
+bunp = require('libs/bump')
+input = require('libs/input')
+inspect = require('libs/inspect')
+log = require('libs/log')
+
+
+-- Self Made Libraries
+
+
+-- Game Dependent Lua Files (Can work with only this game)
+
+
+-- Game Independent Lua Files  (Can work with any game)
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load() 
+    require('dependencies')
+end
+
+function love.update(dt)
+    require('libs/lovebird').update()
+end
+
+function love.draw()
+
+end
