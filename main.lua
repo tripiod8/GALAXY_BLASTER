50a2ae1e83cc4febe9b4a2d75b5e4417b4abcb0d
Tripiod8 <trdoty@mail.wvncc.edu>
2020-04-21 23:55:21 -0400

    Scrolling Background

--- main.lua
+++ main.lua
@@ -1,11 +1,24 @@
+
+
 function love.load() 
     require('dependencies')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    bgImage = love.graphics.newImage('assets/graphics/backgrounds/space-background.png')
+    bgImageWidth, bgImageHeight = bgImage:getDimensions()
+    bgOffset = 0
+    scrollSpeed = 40
 end
 
 function love.update(dt)
+    bgOffset = bgOffset + (scrollSpeed * dt)
+    if (bgOffset >= bgImageHeight) then
+        bgOffset = 0
+    end
     require('libs/lovebird').update()
 end
 
 function love.draw()
-
+    love.graphics.scale(2, 2)
+    love.graphics.draw(bgImage, 0, bgOffset - bgImageHeight)
+    love.graphics.draw(bgImage, 0, bgOffset)
 end
