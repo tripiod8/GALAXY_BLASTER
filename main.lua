3ea62707d9e95476eb110745e8628b420c930c72
Tripiod8 <trdoty@mail.wvncc.edu>
2020-04-22 23:17:10 -0400

    Ship is drawn and positioned

--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
 
 
 function love.load() 
+    scale = 2
     require('dependencies')
     love.graphics.setDefaultFilter('nearest', 'nearest')
     bgImage = love.graphics.newImage('assets/graphics/backgrounds/space-background.png')
     bgImageWidth, bgImageHeight = bgImage:getDimensions()
     bgOffset = 0
     scrollSpeed = 40
+
+    ship = Ship:Create()
 end
 
 function love.update(dt)
@@ -14,11 +17,14 @@ function love.update(dt)
     if (bgOffset >= bgImageHeight) then
         bgOffset = 0
     end
+    ship:update(dt)
+    -- web based debugging system
     require('libs/lovebird').update()
 end
 
 function love.draw()
-    love.graphics.scale(2, 2)
+    love.graphics.scale(scale, scale)
     love.graphics.draw(bgImage, 0, bgOffset - bgImageHeight)
     love.graphics.draw(bgImage, 0, bgOffset)
+    ship:draw()
 end
---
+++ ship.lua
@@ -0,0 +1,38 @@
+Ship = {}
+
+Ship.__index = Ship
+
+function Ship:Create(def)
+local this = {
+    x = 0,
+    y = 0,
+    h = 24,
+    w = 16,
+    image = love.graphics.newImage('assets/graphics/spritesheets/ship.png'),
+    animation = nil,
+    shield = nil,
+    health = 5,
+    weapon = nil
+}
+
+local grid = anim8.newGrid(16, 24, this.image:getDimensions())
+this.animation = anim8.newAnimation(grid(3, '1-2'), 0.15)
+
+this.x = (love.graphics.getWidth() / 2) / scale - (this.w/2)
+this.y = (love.graphics.getHeight() - this.h*2) / scale
+
+setmetatable(this, self)
+return(this)
+end
+
+function Ship:update(dt)
+    self.animation:update(dt)
+end
+
+function Ship:draw()
+    self.animation:draw(self.image, self.x, self.y)
+end
+
+function Ship:handleInput()
+
+end
